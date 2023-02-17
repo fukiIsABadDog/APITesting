13ba22cb69205c7c022635fc8c092bc53901fa7d
fukiIsABadDog <89392732+fukiIsABadDog@users.noreply.github.com>
2023-02-17 08:53:16 -0500

    initial

---
+++ HealthCheckAPI/Controllers/UsersController.cs
@@ -0,0 +1,42 @@
+﻿
+using EFcoreTesting.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace HealthCheckAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UsersController : ControllerBase
+{
+	private MaelstromContext _context { get; set; }
+
+	public UsersController(MaelstromContext context)
+	{
+		_context = context;
+	}
+
+	[HttpGet]
+	public ActionResult<IEnumerable<User>> GetUsers()
+	{
+		var usersList = _context.Users.ToList();
+		return usersList;
+	}
+
+    [HttpGet("{id}")]
+    public ActionResult<User> GetUser(int id)
+    {
+	
+        var user= _context.Users.Find(id);
+		if (user == null)
+		{
+			var message = "Sorry that User does not exist.";
+			return new BadRequestObjectResult(message);
+		}
+		else
+		{
+            return user;
+        }
+		
+    }
+}
